a67d9c19985eb09154ff75f13f756312f9f99d6f
SergioT15 <sergiotitov15@gmail.com>
2023-01-16 17:28:32 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+let line = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let tetrisBoard = [
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+  line,
+];
+
+console.log(tetrisBoard);
+
+let j = [
+  [0, 1, 0],
+  [0, 1, 0],
+  [1, 1, 0],
+];
+
+let i = [
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+];
+
+let o = [
+  [1, 1, 0],
+  [1, 1, 0],
+  [0, 0, 0],
+];
+
+let l = [
+  [0, 1, 0],
+  [0, 1, 0],
+  [0, 1, 1],
+];
+
+let z = [
+  [1, 1, 0],
+  [0, 1, 1],
+  [0, 0, 0],
+];
+
+let t = [
+  [1, 1, 1],
+  [0, 1, 0],
+  [0, 0, 0],
+];
+
+let s = [
+  [0, 1, 1],
+  [1, 1, 0],
+  [0, 0, 0],
+];
